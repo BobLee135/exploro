8b46b916761b34cf612bbf6ea66a16e6a19d1c19
Dennis Andersson <dennis.andersson106@gmail.com>
2022-12-13 18:40:13 +0100

    created register fragment

---
+++ app/src/main/java/com/example/exploro/RegisterFragment.java
@@ -0,0 +1,46 @@
+package com.example.exploro;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RegisterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RegisterFragment extends Fragment {
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * @return A new instance of fragment RegisterFragment.
+     */
+    public static RegisterFragment newInstance() {
+        RegisterFragment fragment = new RegisterFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.register_fragment, container, false);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
