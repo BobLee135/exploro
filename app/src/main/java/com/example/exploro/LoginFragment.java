ed489dbf3b577d2f91022a55833988c22214ed57
Dennis Andersson <dennis.andersson106@gmail.com>
2022-12-13 19:00:52 +0100

    Finished redirects between login and register fragment

--- app/src/main/java/com/example/exploro/LoginFragment.java
+++ app/src/main/java/com/example/exploro/LoginFragment.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,9 +38,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.login_fragment, container, false);
-        Button registerBtn = (Button)view.findViewById(R.id.registerRedirectBtn);
-
-
+        Button registerBtn = (Button)view.findViewById(R.id.registerBtn);
 
         RegisterFragment registerFragment = new RegisterFragment();
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
@@ -50,7 +46,7 @@ public class LoginFragment extends Fragment {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 fragmentManager.beginTransaction()
-                        .replace(R.id.loginFragment, registerFragment, "registerFragment")
+                        .replace(R.id.fragmentContainer, registerFragment, "registerFragment")
                         .addToBackStack(null)
                         .commit();
             }
