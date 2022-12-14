8e8540f1d127b834d25fec6d1fcdf7ef29e3b136
Maxthecoder69 <maxakesson98@gmail.com>
2022-12-14 19:02:06 +0100

    Firebase temp classes

---
+++ app/src/main/java/firebase/Trips.java
@@ -0,0 +1,35 @@
+package firebase;
+
+public class Trips {
+    String city, country, place;
+
+    public Trips(String city, String country, String place) {
+        this.city = city;
+        this.country = country;
+        this.place = place;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+}
