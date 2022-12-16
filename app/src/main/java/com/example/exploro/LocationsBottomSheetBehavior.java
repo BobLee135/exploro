499c3dddcaa94f226a7a278b14aa642b0ec2de24
Dennis Andersson <dennis.andersson106@gmail.com>
2022-12-16 01:27:39 +0100

    basic bottom sheet movement finished

--- app/src/main/java/com/example/exploro/LocationsBottomSheetBehavior.java
+++ app/src/main/java/com/example/exploro/LocationsBottomSheetBehavior.java
@@ -4,35 +4,40 @@ import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 
-import com.example.exploro.controllers.MapsActivityController;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 
-public class LocationsBottomSheetBehavior extends BottomSheetBehavior<android.view.View> {
+public class LocationsBottomSheetBehavior extends BottomSheetBehavior {
 
-    public void LocationsFragmentController(Context context) {
-        super.setDraggable(true);
-        //super.setHideable(false);
-        super.setPeekHeight(50);
-        Log.d("TOUCHTEST", "TEST");
+    public LocationsBottomSheetBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
+    public void LocationsFragmentController() {
+    }
+
+    /** Only allow user to drag up and down bottom sheet by touching the correct component
+     *
+     * @param parent
+     * @param child
+     * @param event
+     * @return
+     */
     @Override
     public boolean onInterceptTouchEvent(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull MotionEvent event) {
-        Log.d("TOUCHTEST", "TEST");
-        return super.onInterceptTouchEvent(parent, child, event);
-    }
+        boolean draggable = false;
+        // Check if user is touching the bottom sheet drag button
+        // set draggable to true if they are, and false if they are not
+        View drag = (View)child.findViewById(R.id.bottomSheetDrag);
+        if (drag != null)
+            draggable = (parent.isPointInChildBounds(drag, (int) event.getX(), (int) event.getY())) ? true : false;
+        super.setDraggable(draggable);
 
-    @Override
-    public boolean onTouchEvent(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull MotionEvent event) {
-        Log.d("TEST", "HELP");
-        return super.onTouchEvent(parent, child, event);
+        return super.onInterceptTouchEvent(parent, child, event);
     }
 }
