53b687d33a51cbd58a57285d98b451d72a6ca2bf
Dennis Andersson <dennis.andersson106@gmail.com>
2022-12-16 06:14:51 +0100

    attempt at retrieving data from firebase

--- app/src/main/java/com/example/exploro/controllers/RegisterFragmentController.java
+++ app/src/main/java/com/example/exploro/controllers/RegisterFragmentController.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.exploro.MessageHelper;
 import com.example.exploro.R;
+import com.example.exploro.models.UserModel;
+import com.example.exploro.models.schemas.User;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,8 +26,11 @@ import com.example.exploro.R;
  */
 public class RegisterFragmentController extends Fragment {
 
+    private MessageHelper mMessageHelper;
+
     public RegisterFragmentController() {
         // Required empty public constructor
+        mMessageHelper = new MessageHelper();
     }
 
     /**
@@ -37,13 +47,82 @@ public class RegisterFragmentController extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.register_fragment, container, false);
+
+        // Get views
         Button cancelRegisterBtn = (Button)view.findViewById(R.id.cancelRegistrationBtn);
+        Button registerBtn = (Button)view.findViewById(R.id.registerButton);
+
+        EditText nameInputField = (EditText) view.findViewById(R.id.fullNameInputField);
+        EditText emailInputField = (EditText) view.findViewById(R.id.emailnputField);
+        EditText usernameInputField = (EditText) view.findViewById(R.id.registerUsernameInputField) ;
+        EditText passwordInputField = (EditText) view.findViewById(R.id.registerPasswordInputField);
+        EditText confirmPasswordInputField = (EditText) view.findViewById(R.id.registerConfirmPasswordInputField);
+
 
         LoginFragmentController loginFragmentController = new LoginFragmentController();
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        UserModel userModel = new UserModel(getActivity());
+
+        /**
+         * Start process of creating a new user from input data
+         */
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get values
+                String fullName = nameInputField.getText().toString();
+                String email = emailInputField.getText().toString();
+                String username = usernameInputField.getText().toString();
+                String password = passwordInputField.getText().toString();
+                String confirmPassword = confirmPasswordInputField.getText().toString();
+
+                // Check data validity
+                if (!isValid(fullName, email, username, password, confirmPassword)) {
+                    mMessageHelper.displaySnackbar("Invalid input", 3, "Error", v);
+                    return;
+                }
+
+                final boolean[] usernameAvailable = {true};
+
+                userModel.findUsernameInUsers(username, new UserModel.ResultStatus() {
+                    @Override
+                    public void resultLoaded(List<User> users) {
+                        if (!users.isEmpty() && users.get(0) != null) {
+                            usernameAvailable[0] = false;
+                            Log.d("RESPONSE", users.get(0).getName().toString());
+                            return;
+                        }
+                    }
+                });
+
+                /*
+                // TODO: Check if username and email is available
+                if (!isUsernameAvailable(username))
+                    return -1;
+                if (!isEmailAvailable(email))
+                    return -2;
+*/
+
+                // Create new user in database
+                int resultCode = userModel.createNewUser(fullName, username, email, password);
+
+                // TODO: Handle result code
+                switch (resultCode) {
+                    case 0:
+                        swapFragment(fragmentManager, loginFragmentController);
+                        mMessageHelper.displaySnackbar("User registered", 3, "Correct", v);
+                        break;
+                    case -1:
+                        mMessageHelper.displaySnackbar("Username already taken", 3, "Error", v);
+                        break;
+                    case -2:
+                        mMessageHelper.displaySnackbar("Email already taken", 3, "Error", v);
+                        break;
+                }
+            }
+        });
 
         /**
          * Cancel register button listener
@@ -52,14 +131,45 @@ public class RegisterFragmentController extends Fragment {
         cancelRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainer, loginFragmentController, "loginFragment")
-                        .addToBackStack(null)
-                        .commit();
+                swapFragment(fragmentManager, loginFragmentController);
             }
         });
 
         // Inflate the layout for this fragment
         return view;
     }
+
+    /**
+     *  Validate user input from registration form
+     *
+     * @param fullName - User input name
+     * @param email - User input email
+     * @param username - User input username
+     * @param password - User input password
+     * @param confirmPassword - User input confirm password
+     * @return true if input passes validation, otherwise false
+     */
+    private boolean isValid(String fullName, String email, String username, String password, String confirmPassword) {
+        if (fullName.length() == 0
+                || email.length() == 0
+                || username.length() == 0
+                || password.length() == 0
+                || confirmPassword.length() == 0)
+            return false;
+        return true;
+    }
+
+    /**
+     * Change active fragment
+     *
+     * @param fm - FragmentManager
+     * @param fragmentController - Fragment controller to swap to
+     */
+    private void swapFragment(FragmentManager fm, Fragment fragmentController) {
+        fm.beginTransaction()
+                .replace(R.id.fragmentContainer, fragmentController, "loginFragment")
+                .addToBackStack(null)
+                .commit();
+    }
+
 }
--- app/src/main/java/com/example/exploro/models/UserModel.java
+++ app/src/main/java/com/example/exploro/models/UserModel.java
@@ -1,21 +1,35 @@
 package com.example.exploro.models;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.example.exploro.models.schemas.Trips;
 import com.example.exploro.models.schemas.User;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import androidx.annotation.NonNull;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class UserModel {
     private DatabaseReference db;
     private Activity activity;
 
-    public UserModel(){
+
+    public interface ResultStatus {
+        void resultLoaded(List<User> users);
+    }
+
+    public UserModel(Activity activity){
         db = FirebaseDatabase.getInstance().getReference();
+        //addUser("Dennis", "test", "dennis@gmail.com","898979");
     }
 
     public void addUser(String name, String password, String email, String phoneNumber){
@@ -36,4 +50,61 @@ public class UserModel {
         Trips trips = new Trips(city, country, place);
         db.child("users").child(user).child("trips").push().setValue(trips);
     }
+
+
+    public int createNewUser(String name, String username, String email, String password) {
+
+
+        // TODO: Write new user to database
+
+        return 0;
+    }
+
+    /**
+     * Check for username in database
+     *
+     * @param username - Username to look for
+     * @return found users in ResultStatus
+     */
+    public void findUsernameInUsers(String username, final ResultStatus result) {
+        db.child("users").child(username).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            List<User> users = new ArrayList<>();
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                    return;
+                }
+                Log.d("firebase", String.valueOf(task.getResult().child("name").getValue()));
+                users.add(task.getResult().getValue(User.class));
+                result.resultLoaded(users);
+                return;
+            }
+        });
+    }
+
+    /**
+     * Check for email in database
+     *
+     * @param email - email to look for
+     * @return found users in ResultStatus
+     */
+    public void findEmailInUsers(String email, final ResultStatus result) {
+        db.child("users").child(email).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            List<User> users = new ArrayList<>();
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                    return;
+                }
+                Log.d("firebase", String.valueOf(task.getResult().child("email").getValue()));
+                users.add(task.getResult().getValue(User.class));
+                result.resultLoaded(users);
+                return;
+            }
+        });
+    }
+
+
 }
