91f338c6459cae1ee57e09522c4739604d24283f
boblee <jacques.artale@hotmail.com>
2022-12-17 19:57:30 +0100

    imports

--- app/src/main/java/com/example/exploro/controllers/ApplicationActivityController.java
+++ app/src/main/java/com/example/exploro/controllers/ApplicationActivityController.java
@@ -1,20 +1,12 @@
 package com.example.exploro.controllers;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import androidx.core.view.GravityCompat;
 
 import com.example.exploro.LocationsBottomSheetBehavior;
 import com.example.exploro.R;
